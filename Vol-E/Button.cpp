0033cd0d5d1860b846301789655c63ffe7531af2
ura0aka <bitcoinassassination@gmail.com>
2022-09-23 11:24:15 -0400

    working button state

---
+++ Vol-E/Button.cpp
@@ -0,0 +1,87 @@
+#include "Button.hpp"
+
+Button::Button(float x, float y, float width, float height,
+    sf::Font* font, std::string text,
+    sf::Color idle, sf::Color hover, sf::Color pressed)
+{
+
+    this->buttonState = IDLE;
+
+    this->mButtonShape.setSize(sf::Vector2f(width, height));
+    this->mButtonShape.setPosition(sf::Vector2f(x, y));
+
+    this->mFont = font;
+    this->mText.setFont(*this->mFont);
+    this->mText.setString(text);
+    this->mText.setFillColor(sf::Color::White);
+    this->mText.setCharacterSize(12);
+    this->mText.setPosition(
+        this->mButtonShape.getPosition().x + (this->mButtonShape.getGlobalBounds().width / 2.0f) - this->mText.getGlobalBounds().width / 2.0f,
+        this->mButtonShape.getPosition().y + (this->mButtonShape.getGlobalBounds().height / 2.0f) - this->mText.getGlobalBounds().height / 2.0f
+    );
+
+    this->mIdleColor = idle;
+    this->mHoverColor = hover;
+    this->mPressedColor = pressed;
+
+    this->mButtonShape.setFillColor(this->mIdleColor);
+
+}
+
+Button::~Button()
+{
+
+}
+
+const bool Button::isPressed() const
+{
+    if (this->buttonState == PRESSED) { return true; }
+    else { return false; }
+}
+
+// == MAIN FUNCTIONS ==
+void Button::updateButton(const sf::Vector2f mousePos)
+{
+    // check if mouse is hovering over the button
+    this->buttonState = IDLE;
+    if (this->mButtonShape.getGlobalBounds().contains(mousePos))
+    {
+        this->buttonState = HOVER;
+
+        // check if button is pressed
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            this->buttonState = PRESSED;
+        }
+    }
+
+    switch (this->buttonState)
+    {
+    case(IDLE):
+    {
+        this->mButtonShape.setFillColor(this->mIdleColor);
+        break;
+    }
+    case(HOVER):
+    {
+        this->mButtonShape.setFillColor(this->mHoverColor);
+        break;
+    }
+    case(PRESSED):
+    {
+        this->mButtonShape.setFillColor(this->mPressedColor);
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+}
+
+void Button::renderButton(sf::RenderTarget* targetWin)
+{
+    targetWin->draw(this->mButtonShape);
+    targetWin->draw(this->mText);
+
+}
---
+++ Vol-E/Button.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+enum buttonState
+{
+    IDLE = 0,
+    HOVER,
+    PRESSED
+};
+
+
+class Button
+{
+
+private:
+
+    std::uint32_t buttonState;
+    bool mPressed;
+    bool mHover;
+
+    sf::RectangleShape mButtonShape;
+    sf::Font* mFont;
+    sf::Text mText;
+
+    sf::Color mIdleColor;
+    sf::Color mHoverColor;
+    sf::Color mPressedColor;
+
+
+public:
+    Button(float x, float y, float width, float height,
+        sf::Font* font, std::string text,
+        sf::Color idle, sf::Color hover, sf::Color pressed);
+
+    ~Button();
+
+    const bool isPressed() const;
+
+    // == MAIN FUNCTIONS ==
+    void updateButton(sf::Vector2f mousePos);
+    void renderButton(sf::RenderTarget* targetWin);
+
+};
