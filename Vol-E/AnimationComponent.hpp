733945c3d6badc039cfee7046558bb8f1fa772a5
ura0aka <bitcoinassassination@gmail.com>
2022-09-29 17:21:14 -0400

    working animations

--- Vol-E/AnimationComponent.hpp
+++ Vol-E/AnimationComponent.hpp
@@ -10,23 +10,24 @@ class AnimationComponent
 {
 private:
 
-	static constexpr int nFrames = 14;
-	static constexpr float nHoldTime = 0.1f;
+	static constexpr int nFrames = 12;
+	static constexpr float nHoldTime = 0.07f;
 	int iFrame = 0;
 	float iFrameTime = 0.0f;
 
 	sf::Texture aTexture;
 	sf::IntRect mFrames[nFrames];
-	
+
 public:
+
 	AnimationComponent() = default;
 
 	AnimationComponent(int x, int y, int width, int height)
 	{
-		aTexture.loadFromFile("res/textures/professor_walk_cycle.png");
-		for (int i = 0; i < nFrames; i++)
+		aTexture.loadFromFile("res/textures/drifter_walk_cycle_full.png");
+		for (int i{ 0 }; i < nFrames; i++)
 		{
-			mFrames[i] = { x + i, y, width, height };
+			mFrames[i] = { x + i * width, y, width, height };
 		}
 	}
 
